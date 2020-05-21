3288cfb34b9cd79bfb3daae21221afac518092de
jnorha <45795929+jnorha@users.noreply.github.com>
2020-05-21 17:16:02 -0500

    Update module_prac.py

--- module_prac.py
+++ module_prac.py
@@ -2,7 +2,7 @@
 # ndvi_c takes 4 arguments: The first is your input GTiff, second is what/where you want your output to go, then an integer describing 
 # which band is associated to the Red wavelength, and finally which band is associated with the NIR wavelengths.
 
-import ndvi_module
+import stratos_ndvi
 
 
-ndvi_module.ndvi_c("naip_horicon_1m-0_1.tif", "naip_horicon_NDVIOUT-0_1.tif", 1, 4)
+stratos_ndvi.ndvi_c("naip_horicon_1m-0_1.tif", "naip_horicon_NDVIOUT-0_1.tif", 1, 4)
